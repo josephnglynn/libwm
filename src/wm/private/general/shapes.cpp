109b8910318d249767b032c1f71211117f8bb728
Joseph <56293291+josephnglynn@users.noreply.github.com>
2021-11-21 17:25:18 +0000

    Fixed typo

--- src/wm/private/general/shapes.cpp
+++ src/wm/private/general/shapes.cpp
@@ -6,7 +6,7 @@ namespace flow::shapes
 {
 	void CenterRectangleOnPlane(Rectangle parent, Rectangle* child)
 	{
-		child->x = parent.x + static_cast<int>((parent.width - child->height) / 2);
+		child->x = parent.x + static_cast<int>((parent.width - child->width) / 2);
 		child->y = parent.y + static_cast<int>((parent.height - child->height) / 2);
 	}
 }
